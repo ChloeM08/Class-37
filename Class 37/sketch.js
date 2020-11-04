183f45f56cd58456ea4606a78744d3314a10b523
ChloeM08 <71110736+ChloeM08@users.noreply.github.com>
2020-11-03 17:57:33 -0800

    Add files via upload

---
+++ Class 37/sketch.js
@@ -0,0 +1,26 @@
+var canvas, database;
+
+var playerCount;
+var gameState = 0;
+
+var player, game, form;
+var allPlayers
+
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+
+    if(playerCount === 4){
+        game.update(1)
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
